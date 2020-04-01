5df8b25a159162d75a2d2e4753108fc97c048a64
Samantha Scott <Samanthascott1992@gmail.com>
2020-04-01 08:05:00 -0600

    working on start button

--- script.js
+++ script.js
@@ -1,30 +1,57 @@
 var gameTimer = 20000;
 
-function timerTick() {
+// function timerTick() {
+// for (var i = 0; i < gameTimer; i--) {
+//  ;
 
 
-
-}
-
-
-
+// }
+// };
 var questionList = [
- {q: "When making a selection in jquery, you use $()", a:"t"},
- {q: `"h3" is the most important header tag`, a:"f"},
- {q: "The <p> is to denote the beginning of a new section", a:"f"}
+ [ "When making a selection in jquery, you use:", "a"],
+  [ "The <p> is to denote the beginning of a new section", "c"],
+ ["The first item in an array has an index of:", "c"],
+ ["In which type of brackets do you use to access an array item?", "d"]
 ];
 
 
-var score = 0;
+function startGame() {
+ for (var i = 0; i < questionList.length; i++) {
+    document.getElementById("question").innerHTML = questionList[i][0];
+}
 
-for (var i = 0; i < questions.length; i++) {
- var answer = confirm(questions[i].q);
-   if ((answer === true && questions[i].a === "t") ||
-      (answer === false && questions[i].a === "f")) {
-      score++;
-      alert("Correct!");
-    }
-    else {
-      alert("Wrong!");
-    }
-  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var score = 0;
+
+// for (var i = 0; i < questionList.length; i++) {
+//  var answer = confirm(questions[i].q);
+// //    if ((answer === true && questions[i].a === "t") ||
+// //       (answer === false && questions[i].a === "f")) {
+//       score++;
+//       alert("Correct!");
+//     }
+//     else {
+//       alert("Wrong!");
+//     }
+//   }
